67660c581517ebd68178c9e43ae48e5e9287b0f2
tbswang <gt2017qd@163.com>
2024-01-02 22:58:59 +0800

    feat: minSize in macos

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -9,7 +9,7 @@ class MainFlutterWindow: NSWindow {
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
-
+    self.minSize = NSSize(width: 600, height: 600)
     super.awakeFromNib()
   }
 }
