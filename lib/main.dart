0242970b4f8680a58190083d99598cee740adae7
tbswang <gt2017qd@163.com>
2024-01-08 20:32:01 +0800

    wip: not refresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'sqlite/todo_item.dart';
-import 'sqlite/database_helper.dart';
+import 'sqlite/todo_provider.dart';
 
 // ignore: constant_identifier_names
 const IS_DONE = 1;
@@ -57,92 +54,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<MyHomePage> {
-  var _database;
+  List<TodoItemData> todoItemList = [];
 
   @override
   void initState() {
     super.initState();
-    _initDatabase();
+    _refreshList();
+  }
+
+  _refreshList() async {
+    List<TodoItemData> data = await todoItems();
+    print(data);
+    setState(() {
+      todoItemList = data;
+    });
   }
 
   final _todoController = TextEditingController();
   var showStatus = 0;
 
-  Future<void> _initDatabase() async {
-    final database = await DatabaseHelper.instance.database;
-    setState(() {
-      _database = database;
-    });
-  }
-
-  _reset() async {
-    final db = await _database;
-    await db.execute('DROP TABLE todo_item');
-  }
-
   _showStatus(int status) async {
     showStatus = status;
     setState(() {});
-  }
-
-  _clear() async {
-    final db = await _database;
-    await db.execute('DELETE FROM todo_item');
-  }
-
-  // Define a function that inserts dogs into the database
-  Future<void> insertTodoItem(TodoItemData todoItemData) async {
-    // Get a reference to the database.
-    final db = await _database;
-
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
-    await db.insert(
-      'todo_item',
-      todoItemData.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-  Future<void> updateDog(TodoItemData todoItemData) async {
-    // Get a reference to the database.
-    final db = await _database;
-
-    // Update the given Dog.
-    await db.update(
-      'todo_item',
-      todoItemData.toMap(),
-      // Ensure that the Dog has a matching id.
-      where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [todoItemData.id],
-    );
-  }
-
-  // A method that retrieves all the dogs from the dogs table.
-  Future<List<TodoItemData>> todoItems({int showStatus = -1}) async {
-    // Get a reference to the database.
-    final db = await _database;
-
-    var maps;
-    if (showStatus >= 0) {
-      maps = await db
-          .query('todo_item', where: 'status = ?', whereArgs: [showStatus]);
-    } else {
-      maps = await db.query('todo_item');
-    }
-    // Query the table for all The Dogs.
-
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
-    return List.generate(maps.length, (i) {
-      return TodoItemData(
-        id: maps[i]['id'] as int,
-        title: maps[i]['title'] as String,
-        status: maps[i]["status"] as int,
-      );
-    });
   }
 
   void _addTodoItem(String v) async {
@@ -156,106 +89,152 @@ class _TodoPageState extends State<MyHomePage> {
     setState(() {});
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FutureBuilder(
+  //       future: todoItems(showStatus: showStatus),
+  //       builder:
+  //           (BuildContext context, AsyncSnapshot<List<TodoItemData>> snapshot) {
+  //         if (snapshot.connectionState == ConnectionState.waiting) {
+  //           return const Center(child: CircularProgressIndicator());
+  //         } else if (snapshot.hasError) {
+  //           return Text('Error: ${snapshot.error}');
+  //         } else if (!snapshot.hasData) {
+  //           return const TextField(
+  //               decoration: InputDecoration(
+  //             hintText: "请输入待办事项",
+  //             contentPadding: const EdgeInsets.all(10.0),
+  //           ));
+  //         } else if (snapshot.connectionState == ConnectionState.done) {
+  //           final _todoItemList = snapshot.data ?? [];
+  //           return Scaffold(
+  //             body: Column(children: <Widget>[
+  //               ButtonBar(
+  //                 alignment: MainAxisAlignment.center,
+  //                 children: <Widget>[
+  //                   ElevatedButton(
+  //                       // 选中的时候紫色
+  //                       style: showStatus == 0
+  //                           ? ButtonStyle(
+  //                               backgroundColor:
+  //                                   MaterialStateProperty.all<Color>(
+  //                                       Colors.purple.shade200),
+  //                             )
+  //                           : null,
+  //                       onPressed: () {
+  //                         _showStatus(0);
+  //                       },
+  //                       child: const Text("进行中")),
+  //                   ElevatedButton(
+  //                       // 选中的时候紫色
+  //                       style: showStatus == 1
+  //                           ? ButtonStyle(
+  //                               backgroundColor:
+  //                                   MaterialStateProperty.all<Color>(
+  //                                       Colors.purple.shade200),
+  //                             )
+  //                           : null,
+  //                       onPressed: () {
+  //                         _showStatus(IS_DONE);
+  //                       },
+  //                       child: const Text("已完成")),
+  //                   ElevatedButton(
+  //                     onPressed: () {
+  //                       clear();
+  //                       setState(() {});
+  //                     },
+  //                     child: const Text('重置'),
+  //                   ),
+  //                 ],
+  //               ),
+  //               Expanded(
+  //                 child: ListView.builder(
+  //                   itemCount: _todoItemList.length,
+  //                   itemBuilder: (BuildContext context, int index) {
+  //                     return CheckboxListTile(
+  //                       title: Text(_todoItemList[index].title),
+  //                       value: _todoItemList[index].status == IS_DONE,
+  //                       onChanged: (bool? value) {
+  //                         var data = _todoItemList[index];
+  //                         var fido = TodoItemData(
+  //                             id: data.id,
+  //                             title: data.title,
+  //                             status: data.status == IS_DONE ? 0 : IS_DONE);
+  //                         updateTodoItem(fido);
+  //                         setState(() {});
+  //                       },
+  //                     );
+  //                   },
+  //                 ),
+  //               ),
+  //               TextField(
+  //                 decoration: InputDecoration(
+  //                   hintText: "请输入待办事项",
+  //                   contentPadding: const EdgeInsets.all(10.0),
+  //                   border: OutlineInputBorder(
+  //                     borderRadius: BorderRadius.circular(15.0),
+  //                   ),
+  //                 ),
+  //                 controller: _todoController,
+  //                 onSubmitted: (v) {
+  //                   _addTodoItem(v);
+  //                   // 清空text filed
+  //                   _todoController.clear();
+  //                   // focus 到 text filed
+  //                   FocusScope.of(context).requestFocus(FocusNode());
+  //                 },
+  //               )
+  //             ]),
+  //           );
+  //         } else {
+  //           return const Center(child: Text("Error"));
+  //         }
+  //       });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: todoItems(showStatus: showStatus),
-        builder:
-            (BuildContext context, AsyncSnapshot<List<TodoItemData>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (!snapshot.hasData) {
-            return const TextField(
-                decoration: InputDecoration(
-              hintText: "请输入待办事项",
-              contentPadding: const EdgeInsets.all(10.0),
-            ));
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            final _todoItemList = snapshot.data ?? [];
-            return Scaffold(
-              body: Column(children: <Widget>[
-                ButtonBar(
-                  alignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ElevatedButton(
-                        // 选中的时候紫色
-                        style: showStatus == 0
-                            ? ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.purple.shade200),
-                              )
-                            : null,
-                        onPressed: () {
-                          _showStatus(0);
-                        },
-                        child: const Text("进行中")),
-                    ElevatedButton(
-                        // 选中的时候紫色
-                        style: showStatus == 1
-                            ? ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.purple.shade200),
-                              )
-                            : null,
-                        onPressed: () {
-                          _showStatus(IS_DONE);
-                        },
-                        child: const Text("已完成")),
-                    ElevatedButton(
-                      onPressed: () {
-                        _clear();
-                        setState(() {});
-                      },
-                      child: const Text('重置'),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: _todoItemList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return CheckboxListTile(
-                        title: Text(_todoItemList[index].title),
-                        value: _todoItemList[index].status == IS_DONE,
-                        onChanged: (bool? value) {
-                          var data = _todoItemList[index];
-                          var fido = TodoItemData(
-                              id: data.id,
-                              title: data.title,
-                              status: data.status == IS_DONE ? 0 : IS_DONE);
-                          updateDog(fido);
-                          setState(() {});
-                        },
-                      );
-                    },
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "请输入待办事项",
-                    contentPadding: const EdgeInsets.all(10.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                  controller: _todoController,
-                  onSubmitted: (v) {
-                    _addTodoItem(v);
-                    // 清空text filed
-                    _todoController.clear();
-                    // focus 到 text filed
-                    FocusScope.of(context).requestFocus(FocusNode());
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: todoItemList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return CheckboxListTile(
+                  title: Text(todoItemList[index].title),
+                  value: todoItemList[index].status == IS_DONE,
+                  onChanged: (bool? value) {
+                    var data = todoItemList[index];
+                    var fido = TodoItemData(
+                        id: data.id,
+                        title: data.title,
+                        status: data.status == IS_DONE ? 0 : IS_DONE);
+                    updateTodoItem(fido);
+                    setState(() {});
                   },
-                )
-              ]),
-            );
-          } else {
-            return const Center(child: Text("Error"));
-          }
-        });
+                );
+              }),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "请输入待办事项",
+            contentPadding: const EdgeInsets.all(10.0),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+          controller: _todoController,
+          onSubmitted: (v) {
+            print(v);
+            _addTodoItem(v);
+            // 清空text filed
+            // _todoController.clear();
+            // focus 到 text filed
+            // FocusScope.of(context).requestFocus(FocusNode());
+          },
+        )
+      ],
+    ));
   }
 }
---
+++ lib/sqlite/todo_provider.dart
@@ -0,0 +1,58 @@
+import 'database_helper.dart';
+import 'todo_item.dart';
+
+// Define a function that inserts dogs into the database
+Future<int> insertTodoItem(TodoItemData todoItemData) async {
+  final db = await DatabaseHelper.instance.database;
+  return await db.insert(
+    'todo_item',
+    todoItemData.toMap(),
+  );
+}
+
+Future<int> updateTodoItem(TodoItemData todoItemData) async {
+  final db = await DatabaseHelper.instance.database;
+
+  return await db.update(
+    'todo_item',
+    todoItemData.toMap(),
+    // Ensure that the Dog has a matching id.
+    where: 'id = ?',
+    // Pass the Dog's id as a whereArg to prevent SQL injection.
+    whereArgs: [todoItemData.id],
+  );
+}
+
+clear() async {
+  final db = await DatabaseHelper.instance.database;
+  return await db.execute('DELETE FROM todo_item');
+}
+
+_reset() async {
+  final db = await DatabaseHelper.instance.database;
+  return await db.execute('DROP TABLE todo_item');
+}
+
+// A method that retrieves all the dogs from the dogs table.
+Future<List<TodoItemData>> todoItems({int showStatus = -1}) async {
+  // Get a reference to the database.
+  final db = await DatabaseHelper.instance.database;
+
+  var maps;
+  if (showStatus >= 0) {
+    maps = await db
+        .query('todo_item', where: 'status = ?', whereArgs: [showStatus]);
+  } else {
+    maps = await db.query('todo_item');
+  }
+  // Query the table for all The Dogs.
+
+  // Convert the List<Map<String, dynamic> into a List<Dog>.
+  return List.generate(maps.length, (i) {
+    return TodoItemData(
+      id: maps[i]['id'] as int,
+      title: maps[i]['title'] as String,
+      status: maps[i]["status"] as int,
+    );
+  });
+}
